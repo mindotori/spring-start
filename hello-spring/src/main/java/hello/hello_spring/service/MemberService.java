fa640c6fbfd0c8df45b0afacab67ce4e0ad2d0f1
mindotori <148763440+mindotori@users.noreply.github.com>
2024-12-10 17:32:08 +0900

    회원 서비스 테스트

--- hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
@@ -9,7 +9,11 @@ import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) { // DI(외부에서 넣어주도록 설정)
+        this.memberRepository = memberRepository;
+    }
 
     //회원 가입
     public Long join(Member member){
